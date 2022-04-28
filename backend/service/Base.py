12f6bdd251e5f23e1d103d5d6ad7a1f3c7b4fe6c
Thuandz <thuanhppro12@gmail.com>
2022-04-28 10:57:38 +0700

    add query text

--- backend/service/Base.py
+++ backend/service/Base.py
@@ -1,29 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
 from model.request import RequestSearch
-import http3
+import http3, string, random
+from typing import Dict, List, Optional
+
 class BaseService:
     def __init__(self):
-        self.client = http3.AsyncClient()
+        pass
     
 
     def doQuery(self,url:str):
-        htmlText=requests.get(url).text
-        soup = BeautifulSoup(htmlText, 'html.parser')
+        try:
+            htmlText=requests.get(url).text
+            soup = BeautifulSoup(htmlText, 'html.parser')
+        except:
+            return None
         return soup
 
-    async def asyncDoQuery(self,url):
-        htmlText= await self.client.get(url)
-        soup = BeautifulSoup(htmlText.text, 'html.parser')
+    async def asyncDoQuery(self,url,sleep_range:float=0):
+        client = http3.AsyncClient()
+        try:
+            if sleep_range > 0:
+                await asyncio.sleep(random.random())
+            htmlText= await client.get(url)
+            soup = BeautifulSoup(htmlText.text, 'html.parser')
+        except:
+            return None
         return soup
 
+    def remove_accents(self,input_str):
+        s1 = u'ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
+        s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
+        s = ''
+       # print input_str.encode('utf-8')
+        for c in input_str:
+            if c in s1:
+                s += s0[s1.index(c)]
+            else:
+                s += c
+        return s
+    def matching(self,query:str,target:str):
 
+        target = target.lower().replace(" ","")
+        target=target.translate(str.maketrans('', '', string.punctuation))
+        target = self.remove_accents(target)
+        query = query.lower()
+        query=query.translate(str.maketrans('', '', string.punctuation))
+        query = self.remove_accents(query)
+        query = query.split(" ")
+        query = list(filter(None, query))
+        for q in query:
+            if q in target:
+               # print(True,q,target)
+                return True
+       # print(False)
+        return False
+    def filterQuery(self,query:str,candidates:List[dict]):
+        results = []
+        for candidate in candidates:
+            if self.matching(query,candidate["title"]):
+                results.append(candidate)
+        return results
     async def process(self,req:RequestSearch):
         raise NotImplementedError
     def rewriteQuery(self,req:RequestSearch)-> str:
         # take request body and rewrite query statement with coresponding web pages
         # return the url of the right webpage
         raise NotImplementedError 
-
-    
         
--- backend/service/ToanMathService.py
+++ backend/service/ToanMathService.py
@@ -1,7 +1,7 @@
 from .Base import BaseService
 from model.request import RequestSearch
 from model.responses import Response
-
+import asyncio
 
 class ToanMathService(BaseService):
     def __init__(self,):
@@ -23,13 +23,13 @@ class ToanMathService(BaseService):
                 url = f"https://toanmath.com/de-thi-hsg-toan-{req.grade}" 
         if req.page > 1 and url != None:
             url += "/page/"+str(req.page)
+        
         return url
-
-    async def process(self,req:RequestSearch):
-        if req.text is None or req.text == "":
-            results = []
-            url = self.rewriteQuery(req)
-            soup = await self.asyncDoQuery(url)
+    def parser_html(self,soup):
+        
+        results=[]
+        try:
+            #print(type(soup))
             records = soup.find_all('div', class_='mh-col-1-2 mh-posts-grid-col clearfix')
             for record in records:
                 date = record.find("div",class_='mh-meta entry-meta').find("a")
@@ -41,6 +41,45 @@ class ToanMathService(BaseService):
                 result = Response(title=title,link=link,date=date).dict()
                 results.append(result)
             return results
-        else:
+        except Exception as e:
+            print(e)
+            return results
 
-            return []
+    async def process(self,req:RequestSearch):
+        results = []
+        if req.text is None or req.text == "":
+            
+            url = self.rewriteQuery(req)
+            if url == None:
+                return results
+            soup = await self.asyncDoQuery(url)
+            if soup == None:
+                raise HTTPException(404,"Not found, try again later")
+            results = self.parser_html(soup)
+            return results
+        else:
+            candidates = []
+            page = req.page
+            list_soup = []
+            for i in range((page-1)*5,page*5):
+                if i == 0 :continue
+                req.page=i
+                url = self.rewriteQuery(req)
+                if url == None:
+                    continue
+                fut = self.asyncDoQuery(url)
+                list_soup.append(fut)
+            temps = await asyncio.gather(*list_soup)
+            
+            for temp in temps:
+                
+                if temp == None: continue
+                else:
+                    candidates.append(temp)
+            for x in candidates:
+                #print(type(x))
+                results+=self.parser_html(x)
+           # candidates = [ for x in candidates]
+            results = self.filterQuery(req.text,results)
+
+            return results
